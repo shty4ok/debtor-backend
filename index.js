b6e6ecb9c49cce6017aaea19874235d3b9b147aa
Aleksey <gurin.aleksey@gmail.com>
2020-03-19 18:34:53 +0300

    response token & add .gitignore

--- index.js
+++ index.js
@@ -2,25 +2,35 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const jwt = require('jsonwebtoken');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(cors());
-const urlencodedParser = bodyParser.urlencoded({extended: false});
 // support parsing of application/json type post data
 app.use(bodyParser.json());
-
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post('/api/auth', urlencodedParser, (req, res, next) => {
   const user = {
     login: 'admin',
-    password: 'admin'
+    password: 'admin',
+    token: ''
   };
+
   if (req.body.login === user.login && req.body.password === user.password ) {
-    res.send(true);
+    jwt.sign( req.body.login, 'KOKO', (err, token)=> {
+      this.user.token = token;
+      console.dir(this.user.token);
+      res.send(token);
+    });
   } else {
-    res.send(false);
+    res.send(null);
   }
 });
+app.get('/api/data', (req, res) => {
+  res.send('Hello');
+})
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
