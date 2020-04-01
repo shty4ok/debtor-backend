f32a78b908f69415c8a421f8f26ff36ce14209ee
Aleksey Gurin <gurin.aleksey@gmail.com>
2020-04-01 16:16:06 +0300

    try to verify jwt

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 const app = express();
+const jwtsecret = 'KOKO';
 
 let dataArray = [{
+    id: 1,
     name: 'Aleksey',
     amount: '5000',
     date: '20-20-33'
   },
   {
+    id: 2,
     name: 'Semen',
     amount: '2000',
     date: '2-22-33'
   },
   {
+    id: 3,
     name: 'Oleg',
     amount: '1200',
     date: '3-3-33'
   }];
 const user = {
   login: 'admin',
-  password: 'admin'
+  password: 'admin',
+  token: '1A2b3C4d5E6f7G8h9IAgBKClD'
 };
 
 app.use(cors());
@@ -31,13 +37,9 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/api/auth', urlencodedParser, (req, res, next) => {
-  const user = {
-    login: 'admin',
-    password: 'admin',
-  };
 
   if (req.body.login === user.login && req.body.password === user.password ) {
-    jwt.sign( req.body.login, 'KOKO', (err, token)=> {
+    jwt.sign( req.body.login, jwtsecret, (err, token)=> {
       res.json({
         status: true,
         token: token
@@ -55,9 +57,17 @@ app.post('/api/debts', urlencodedParser, (req, res, next) => {
   res.status(201).json(req.body);
 });
 app.get('/api/debts', function (req, res) {
-  res.json(dataArray);
+  // jwt.verify(req.headers.authorization, jwtsecret, (err, decode) => {
+  //   console.log(decode);
+  // });
+  if(req.headers.authorization === user.token) {
+    res.json(dataArray);
+  } else {
+    res.end();
+  }
 });
 app.delete('/api/debts/:id', urlencodedParser, (req, res, next) => {
+
   const newArr = dataArray.filter(( obj ) => {
     return obj !== req.body;
   });
@@ -68,3 +78,5 @@ app.delete('/api/debts/:id', urlencodedParser, (req, res, next) => {
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
+
